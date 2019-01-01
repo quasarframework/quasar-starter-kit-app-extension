978aebedb2a60c530d3b3ac706d8c0c9730ea909
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-01-01 16:59:11 +0200

    Update install.js

--- template/install.js
+++ template/install.js
@@ -1,7 +1,7 @@
 /**
  * Quasar App Extension install script
  *
- * API: https://github.com/quasarframework/quasar/blob/dev/app/lib/app-extension/InstallAPI.js
+ * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/InstallAPI.js
  */
 
 module.exports = function (api) {
