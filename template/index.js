7a437c58a59461000565a0584aebfc05e45b27eb
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-01-01 16:59:00 +0200

    Update index.js

--- template/index.js
+++ template/index.js
@@ -2,7 +2,7 @@
  * Quasar App Extension index/runner script
  * (runs on each dev/build)
  *
- * API: https://github.com/quasarframework/quasar/blob/dev/app/lib/app-extension/IndexAPI.js
+ * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
  */
 
 module.exports = function (api, ctx) {
